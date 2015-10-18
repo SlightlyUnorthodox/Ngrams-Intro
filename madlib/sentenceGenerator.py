fb0ab8ae9c7a58ba58414b8866b23651470d1068
Dax <gerts.dax1@gmail.com>
2015-10-17 23:19:35 -0400

    sentence generator construct

---
+++ madlib/sentenceGenerator.py
@@ -0,0 +1,22 @@
+import nltk
+
+class sentenceGenerator():
+
+		def sentenceGenerator(toggle):
+			#Test procedure (no real sentence generation)
+			if toggle == 1:
+
+			#Method1:
+			elif toggle == 2:
+
+			#Method2:
+			elif toggle == 3:
+
+			#Bad call to 'toggle'
+			else:
+				print "Invalid test method"
+
+			return
+
+
+		def wordReplacer(sentence):
