5a08edda0db56dc697db1f13df9591e1c408b0c6
Dax <gerts.dax1@gmail.com>
2015-10-18 00:07:18 -0400

    fleshed out word replace

--- madlib/sentenceGenerator.py
+++ madlib/sentenceGenerator.py
@@ -19,4 +19,24 @@ class sentenceGenerator():
 			return
 
 
-		def wordReplacer(sentence):
+		def wordReplacer(sentencePOS):
+			#Picks a single word out of a sentence to replace
+			
+			#set random word choice start point
+			num = np.random.randint(len(sen),size=1)[0]
+
+			#process argument vectors
+			sentencePOS = sentencePOS.split()			
+
+			#loop until appropriate word is detected
+			stop = "false"
+			while stop == "false":
+				#add extra parts of speech if necessary
+				if sentencePOS[num] == "adjective" || sentencePOS[num] == "noun" || sentencePOS[num] == "verb":
+					stop = "true"
+				else:
+					#if not correct part of speech move to next word
+					num = (num + 1) % len(sentencePOS)
+
+			#return word replacement index
+			return num
